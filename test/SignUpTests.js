9742c7beca189ac5f4f297459b9b78d3601bd496
jasminhusadzic <jasmin.h@aleacontrol.com>
2021-01-07 09:56:42 +0100

    add v4 to uuid4

--- test/SignUpTests.js
+++ test/SignUpTests.js
@@ -1,4 +1,4 @@
-import uuidv4 from "uuid/v4";
+import {v4 as uuidv4} from "uuid";
 import { t } from "testcafe";
 import ProfilePage from "../pages/ProfilePage";
 import SignUpPage from "../pages/SignUpPage"
